d4584ad8546b7143a922865e273716d46ed69d71
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-07-27 19:33:50 -0300

    Correção no Patch

--- script.js
+++ script.js
@@ -1,172 +1,110 @@
-	**{
-		margin: 0;
-	    padding: 0; 
-
-	}
-
-
-	header{
-		text-align: center;
-	}
-
-	body{
-		background-color: #f5e8c6;
-	}
-
-	.inicio{
-		height: 100vh;
-	}
-
-
-
-	.contanie-img{
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-		border-style: solid;
-		height: 100%;
-		
-	}
-
-	.contanie-img .img-day{
-		width: 100%;
-		height: 100%;
-	}
-
-	.contanie-img .dados-img{
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		margin-top: 10%;
-		margin-left: 20px;
-		width: 90%;
-		height: 100%;
-		grid-area: 1 2 1 2;		
-	}
-
-	.contanie-img .dados-img button{
-	    background-color: #ef3353; /* Cor de fundo */
-	    color: white; /* Cor do texto */
-	    border: none; /* Remover borda */
-	    padding: 10px 20px; /* Espaçamento interno */
-	    text-align: center; /* Alinhamento do texto */
-	    text-decoration: none; /* Remover sublinhado */
-	    display: inline-block; /* Exibir como um bloco */
-	    font-size: 18px; /* Tamanho da fonte */
-	    width: 65%;
-	    
-	    cursor: pointer; /* Cursor ao passar */
-	    border-radius: 20px; /* Raio da borda para arredondar */
-	    transition: background-color 0.3s ease; /* Transição suave de cor de fundo */
-	    box-shadow: 0px 0px 0px 2px;
-	    margin: auto 4px;
-	    margin-top: 10px;		
-	}
-
-	.cards-img{
-	 	display: grid;
-	    grid-template-columns: 1fr;
-	    grid-template-rows: 1fr;    
-	    width: 90%;
-	    max-width: 1140px;
-	    min-width: 1fr;
-	    height: 100vh;
-	    margin: auto; 
-	    margin-bottom: 20px;  	    
-	    margin-top: 10%;
-	}
-
-	.cards-img .titulo, .texto-titulo{
-		text-align: center;
-	}
-
-	.cards-img .cards-blocos{		
-		gap: 10px;
-	}
-
-	.cards-img ul{
-			
-	}
-
-	.cards-img ul li{
-		display: inline-flex;
-		list-style-type: none;		
-		margin: 10px;
-		align-items: center;	
-		border-style: solid;
-		padding: 5px;	
-	}
-
-	.cards-img ul li span{		
-		
-	}
-
-	.cards-img ul li .dados-img{
-		display: grid;
-		gap: 0px;
-		margin-left: 20px;		
-		width: 520px;
-		max-width: 520px;
-	}
-
-	.cards-img ul li .dados-img #titulo-img{
-		font-weight: bolder;		
-		font-size: 30px;
-	}
-
-	.cards-img ul li .dados-img .descricao-img{
-		font-size: 22px;
-		font-style: italic;
-		max-height: 4.8em;
-	}
-
-	.cards-img ul li .dados-img button{
-	    background-color: #ef3353; /* Cor de fundo */
-	    color: white; /* Cor do texto */
-	    border: none; /* Remover borda */
-	    padding: 10px 20px; /* Espaçamento interno */
-	    text-align: center; /* Alinhamento do texto */
-	    text-decoration: none; /* Remover sublinhado */
-	    display: inline-block; /* Exibir como um bloco */
-	    font-size: 18px; /* Tamanho da fonte */
-	    width: 65%;
-	    
-	    cursor: pointer; /* Cursor ao passar */
-	    border-radius: 20px; /* Raio da borda para arredondar */
-	    transition: background-color 0.3s ease; /* Transição suave de cor de fundo */
-	    box-shadow: 0px 0px 0px 2px;
-	    margin: auto 4px;
-	    margin-top: 10px;				
-	}
-
-
-	.cards-img ul li img{
-		max-width: 420px;		
-	}
-
-
-	.cards-img ul li:hover{
-
-	}
-
-	.cards-img ul li img:hover{
-	
-	}
-
+$(function(){
+	const url_key = "https://api.nasa.gov/planetary/apod?api_key=LAgHhV4J8lHUU7pvT9A0rwVJpz3teTvJ3vfcZW2W";
 	
 
-	@media (max-width: 767px){
-		.contanie-img{
-			grid-template-columns: 1fr;
-			grid-auto-flow: 1fr;
-			height: auto;
-		}
+	// Obtendo a data atual
+	let Data_atual = new Date();
 
-		.contanie-img .dados-img button{
-			margin-bottom: 10px;
-		}
+	// Obtendo o mês atual e adicionando 1 para corrigir a base zero
+	var mes = Data_atual.getMonth() + 1;
 
-		.contanie-img .img-day{
-			
-		}
+	// Formatando o mês com zero à esquerda se necessário
+	var mesFormatado = mes.toString().padStart(2, '0');
 
-	}
+	// Formatando o dia com zero à esquerda se necessário
+	var dia = (Data_atual.getDate()-6).toString().padStart(2, '0');
+
+	// Criando a data futura no formato YYYY-MM-DD
+	let Data_futura = Data_atual.getFullYear().toString() + "-" + mesFormatado + "-" + dia;
+
+	const formatter = new Intl.DateTimeFormat('pt-BR', {
+	  year: 'numeric',
+	  month: 'long',
+	  day: 'numeric',
+	  weekday: 'long' // Opcional, para incluir o nome do dia da semana
+	}); //  API Intl.DateTimeFormat
+
+
+	// Fazendo a requisição GET
+	fetch(url_key+"&start_date="+Data_futura)		
+	  .then(response => {
+	    // Verifica se a resposta foi bem-sucedida
+	    if (!response.ok) {
+	      throw new Error('Network response was not ok');
+	    }
+	    // Retorna a resposta como JSON
+	    return response.json();
+	  })
+	  .then(data => {	  	
+	  	if(Array.isArray(data)){
+	  		Data_atual = formatter.format(Data_atual);
+
+	  		//Exibindo a imagen do dia
+	  		$('#dia-img').text(Data_atual.toString());
+	  		$('#titulo-img').text(data[6].title);	
+	  		$('#descricao-img').text(data[6].explanation);
+	  		$('#autor').text(data[6].copyright);
+	  		$('#img-day').attr('src',data[6].url);
+	  		//
+
+	  		//preenchimento da img da semana 
+	  		for (const index in data){
+	  			var tagImg = "#img-" + index;
+	  			var tagDados = "li .dados-img:nth-of-type(" + index + ")";
+	  			$('li .dados-img:nth-of-type(3)')
+
+
+	  			$(tagImg).attr('src',data[index].url);
+	  			$('li .dados-img:nth-of-type(3)')
+	  		}
+
+
+	  				// texto.textContent = originalText.substring(0, previewLength) + '...';
+		        // <li class="img-6">
+		        //     <img class="img-6" id="img-5" src="" alt="Imagem 6">
+		        //     <div class="dados-img" id="dados-img">
+		        //     	<span id="titulo-img" style="">Nome imagen</span><br/>	
+			      //       <span class="descricao-img" style="">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</span><br/>
+			      //       <button>Veja mais!</button></br>
+			      //       <span class="data-img" id="data-img" style="margin-left: 15px;">sábado, 27 de julho de 2024</span><br/>
+		        // 	</div>
+		        // </li>	  		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  	}
+	  })
+	  .catch(error => {
+	    // Trata erros de rede ou problemas na requisição
+	    console.error('There was a problem with the fetch operation:', error);
+	});
+
+	$('#link-post').click(
+		function(){
+		 navigator.clipboard.writeText("google.com")
+        .then(() => {
+            $('#link-post').text("Link Copiado!");
+            $('#link-post').css('background-color','#ef6383');// background-color: #ef3353
+        })
+        .catch(err => {
+            console.error('Falha ao copiar o texto: ', err);
+        });
+		}
+	);  
+})
+
+
