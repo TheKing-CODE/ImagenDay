fbfba9f3aa508cd104f9546ba1af27107dd2476b
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-07-27 22:05:32 -0300

    Add Descrições
    Add  descrições e novos estilos

---
+++ script/script.js
@@ -0,0 +1,91 @@
+$(function(){
+	const url_key = "https://api.nasa.gov/planetary/apod?api_key=LAgHhV4J8lHUU7pvT9A0rwVJpz3teTvJ3vfcZW2W";
+	
+
+	// Obtendo a data atual
+	let Data_atual = new Date();
+
+	// Obtendo o mês atual e adicionando 1 para corrigir a base zero
+	var mes = Data_atual.getMonth() + 1;
+
+	// Formatando o mês com zero à esquerda se necessário
+	var mesFormatado = mes.toString().padStart(2, '0');
+
+	// Formatando o dia com zero à esquerda se necessário
+	var dia = (Data_atual.getDate()-6).toString().padStart(2, '0');
+
+	// Criando a data futura no formato YYYY-MM-DD
+	let Data_futura = Data_atual.getFullYear().toString() + "-" + mesFormatado + "-" + dia;
+
+	const formatter = new Intl.DateTimeFormat('pt-BR', {
+	  year: 'numeric',
+	  month: 'long',
+	  day: 'numeric',
+	  weekday: 'long' // Opcional, para incluir o nome do dia da semana
+	}); //  API Intl.DateTimeFormat
+
+
+	// Fazendo a requisição GET
+	fetch(url_key+"&start_date="+Data_futura)		
+	  .then(response => {
+	    // Verifica se a resposta foi bem-sucedida
+	    if (!response.ok) {
+	      throw new Error('Network response was not ok');
+	    }
+	    // Retorna a resposta como JSON
+	    return response.json();
+	  })
+	  .then(data => {	  	
+	  	if(Array.isArray(data)){
+	  		Data_atual = formatter.format(Data_atual);
+
+	  		//Exibindo a imagen do dia
+	  		$('#dia-img').text(Data_atual.toString());
+	  		$('#titulo-img').text(data[6].title);	
+	  		$('#descricao-img').text(data[6].explanation);
+	  		$('#autor').text(data[6].copyright);
+	  		$('#img-day').attr('src',data[6].url);
+	  		//
+
+	  		//preenchimento da img da semana 
+	  		for (const index in data){
+	  			var tagImg = "#img-" + index;
+				var tagTituloImg = "#titulo-img-" + index;
+				var tagDescricaoImg = "#descricao-img-" + index;
+				var tagDataImg = "#data-img-" + index;
+				var descricaoImg = data[index].explanation;
+				descricaoImg = descricaoImg.slice(0, 150) + "...";
+				
+
+	  			var tagDados = "li .dados-img:nth-of-type(" + index + ")";
+	  			$('li .dados-img:nth-of-type(3)')
+
+
+	  			$(tagImg).attr('src',data[index].url);
+	  			$(tagTituloImg).text(data[index].title);
+				$(tagDescricaoImg).text(descricaoImg);
+				$(tagDataImg).text(data[index].date);
+
+	  		}
+	  	}
+	  })
+	  .catch(error => {
+	    // Trata erros de rede ou problemas na requisição
+	    console.error('There was a problem with the fetch operation:', error);
+	});
+
+	$('#link-post').click(
+		function(){
+		 navigator.clipboard.writeText("google.com")
+        .then(() => {
+            $('#link-post').text("Link Copiado!");
+            $('#link-post').css('background-color','#ef6383');// background-color: #ef3353
+        })
+        .catch(err => {
+            console.error('Falha ao copiar o texto: ', err);
+        });
+		}
+	);  
+})
+
+
