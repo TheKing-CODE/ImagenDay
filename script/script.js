4cdbe47a7ecd246f069e2f506b9292745a7fb197
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-07-29 11:30:25 -0300

    Update script.js
    Add nova imagens para nova secão

--- script/script.js
+++ script/script.js
@@ -1,122 +1,341 @@
-$(function(){
-	const url_key = "https://api.nasa.gov/planetary/apod?api_key=LAgHhV4J8lHUU7pvT9A0rwVJpz3teTvJ3vfcZW2W";
+	**{
+		margin: 0;
+	    padding: 0; 
+
+	}
+
+
+	header{
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 25px;
+		border-bottom: solid;
+		font-family: "Playwrite NG Modern", cursive;
+		font-optical-sizing: auto;
+		font-style: normal;
+	}
+
+	header .img-principal{
+		width: 10%;
+		height: 10%;
+	}
+
+	body{
+		background-color: #c3b199;
+	}
+
+	.main{
+		display: grid;
+	}
+
+	.introducao{
+		display: grid;
+		grid-template-columns: 1fr 1fr;	
+		align-items: center;		
+		gap: 10px;
+		margin-bottom: 10px;
+		max-width: 1140px;
+		margin: auto;		
+		margin-bottom: 5%;
+		background-color: #3a2d19;
+		color: antiquewhite;
+		
+	}
+
+	.introducao .galeria-img {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;	
+		align-items: center;
+		max-width: 1140px;
+	    min-width: 1fr;	    		
+		gap: 10px;
+		padding: 5px;
+	}
+
+
+	.introducao .introducao-texto .titulo-introducao{
+		font-family: "Playwrite NG Modern", cursive;
+		font-optical-sizing: auto;
+		font-style: normal;
+	}
+
+	.introducao .introducao-texto h1{
+		color: #e8373e;
+	}
+
+	.introducao .introducao-texto h1,h2{
+		text-align: center;		
+	}
+
+
+	.introducao .galeria-img img{
+		width: 100%;		
+	}
+
+	.introducao .introducao-texto a{
+		position: relative;
+		left: 45%;
+		color: antiquewhite;		
+		font-size: 30px;
+		text-decoration: none;
+	}
 	
+	.introducao .introducao-texto a:hover{
+		color: #e8373e;
+	}
 
-	// Obtendo a data atual
-	let Data_atual = new Date();
-
-	let dataFormatada = Data_atual.toLocaleString('en-US', { timeZone: 'America/New_York' });
-	Data_atual = new Date(dataFormatada);
-
-	// Obtendo o mês atual e adicionando 1 para corrigir a base zero
-	var mes = Data_atual.getMonth() + 1;
-
-	// Formatando o mês com zero à esquerda se necessário
-	var mesFormatado = mes.toString().padStart(2, '0');
-
-	// Formatando o dia com zero à esquerda se necessário
-	var dia = (Data_atual.getDate()-6).toString().padStart(2, '0');
-
-	// Criando a data futura no formato YYYY-MM-DD
-	let Data_futura = Data_atual.getFullYear().toString() + "-" + mesFormatado + "-" + dia;
-
-	const formatter = new Intl.DateTimeFormat('pt-BR', {
-	  year: 'numeric',
-	  month: 'long',
-	  day: 'numeric',
-	  weekday: 'long' // Opcional, para incluir o nome do dia da semana
-	}); //  API Intl.DateTimeFormat
+	.inicio{
+		height: 100vh;
+		max-width: 1140px;
+		margin: auto;		
+	}
 
 
-	console.log(Data_atual);
+	.contanie-img{
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		height: 100%;
+		padding-bottom: 10px;
+		border-bottom: solid;
+	}
+
+	.contanie-img .img-day{
+		width: 100%;
+		height: 100%;
+	}
+
+	.contanie-img .dados-img{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 10%;
+		margin-left: 20px;
+		width: 90%;
+		height: 100%;
+		grid-area: 1 2 1 2;		
+	}
+
+	.contanie-img .dados-img{
+		color: #25020f;
+		font-size: 18px;
+	}
+
+	.contanie-img .dados-img .titulo-img{
+		color: #e8373e;
+	}
 
 
-	// Fazendo a requisição GET
-	fetch(url_key+"&start_date="+Data_futura)		
-	  .then(response => {
-	    // Verifica se a resposta foi bem-sucedida
-	    if (!response.ok) {
-	      throw new Error('Network response was not ok');
-	    }
-	    // Retorna a resposta como JSON
-	    return response.json();
-	  })
-	  .then(data => {	  	
-	  	if(Array.isArray(data)){
-	  		Data_atual = formatter.format(Data_atual);
+	.contanie-img .dados-img button{
+	    background-color: #ef3353; /* Cor de fundo */
+	    color: white; /* Cor do texto */
+	    border: none; /* Remover borda */
+	    padding: 10px 20px; /* Espaçamento interno */
+	    text-align: center; /* Alinhamento do texto */
+	    text-decoration: none; /* Remover sublinhado */
+	    display: inline-block; /* Exibir como um bloco */
+	    font-size: 18px; /* Tamanho da fonte */
+	    width: 65%;
+	    
+	    cursor: pointer; /* Cursor ao passar */
+	    border-radius: 20px; /* Raio da borda para arredondar */
+	    transition: background-color 0.3s ease; /* Transição suave de cor de fundo */
+	    box-shadow: 0px 0px 0px 2px;
+	    margin: auto 4px;
+	    margin-top: 10px;		
+	}
+
+	.cards-img{
+	 	display: grid;
+	    grid-template-columns: 1fr;
+	    grid-template-rows: 1fr;    
+	    width: 90%;
+	    max-width: 1140px;
+	    min-width: 1fr;
+	    height: auto;
+	    margin: auto; 
+	    margin-bottom: 20px;  	    
+	    margin-top: 5%;	    
+	}
+
+	.cards-img .titulo{
+		font-family: "Playwrite NG Modern", cursive;
+		font-optical-sizing: auto;
+		font-style: normal;			
+	}
+
+	.cards-img .img-principal-2{
+		margin: auto;
+	}
+
+	.cards-img .titulo, .texto-titulo{
+		text-align: center;
+	}
+
+	.cards-img .cards-blocos{		
+		height: auto;
+		
+	}
+
+	.cards-img ul li{
+		display: inline-flex;
+		list-style-type: none;		
+		margin: 10px;
+		align-items: center;	
+		border-style: solid;
+		border-color: #e8373e;
+		padding: 5px;	
+		background-color: #3a2d19;
+		color: #ffffff;
+	}
+
+	.cards-img ul li .dados-img{
+		display: grid;
+		gap: 0px;
+		margin-left: 20px;		
+		width: 520px;
+		max-width: 520px;
+	}
+
+	.cards-img ul li .dados-img .titulo-img{
+		font-weight: bolder;		
+		font-size: 30px;
+		color: #e8373e;
+	}
+
+	.cards-img ul li .dados-img .descricao-img{
+		font-size: 22px;
+		font-style: italic;
+		max-height: 4.8em;
+	}
+
+	.cards-img ul li .dados-img button{
+	    background-color: #ef3353; /* Cor de fundo */
+	    color: white; /* Cor do texto */
+	    border: none; /* Remover borda */
+	    padding: 10px 20px; /* Espaçamento interno */
+	    text-align: center; /* Alinhamento do texto */
+	    text-decoration: none; /* Remover sublinhado */
+	    display: inline-block; /* Exibir como um bloco */
+	    font-size: 18px; /* Tamanho da fonte */
+	    width: 65%;
+	    
+	    cursor: pointer; /* Cursor ao passar */
+	    border-radius: 20px; /* Raio da borda para arredondar */
+	    transition: background-color 0.3s ease; /* Transição suave de cor de fundo */
+	    box-shadow: 0px 0px 0px 2px;
+	    margin: auto 4px;
+	    margin-top: 10px;				
+	}
+
+
+	.cards-img ul li img{
+		max-width: 420px;		
+	}
+
+
+	footer{
+		padding: 5px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 50px;
+		max-height: 50px;
+		background-color: #ef3353;
+		color: aliceblue;
+		width: 1140px;
+		max-width: 1140px;
+		margin: auto;
+		margin-top: 2%;
+	}
+	
+	footer a{
+		text-decoration: none;
+		color: antiquewhite;
+		margin-right: 10px;
+		font-size: 30px;
+	}
+	
+	footer a:hover{
+		color: black;
+	}
+
+	@media (max-width: 767px){
+
+		**{
+			padding: 0px;
+			margin: 0px;
 			
-	  		//Exibindo a imagen do dia
-	  		$('#dia-img').text(Data_atual.toString());
-	  		$('#titulo-img').text(data[6].title);	
-	  		$('#descricao-img').text(data[6].explanation);
-	  		$('#autor').text(data[6].copyright);
-	  		$('#img-day').attr('src',data[6].url);
-			var urlDay = data[6].url; 
-			if(urlDay.includes("youtube")){
-				  $('#img-day').css('display','none');
-				  $('#video-principal').css('display','');
-				  $('#video-link').attr('src',urlDay);
-				  console.log(urlDay);
-			  }
-  
-	  		//
-
-	  		//preenchimento da img da semana 
-	  		for (const index in data){
-	  			var tagImg = "#img-" + index;
-				var tagTituloImg = "#titulo-img-" + index;
-				var tagDescricaoImg = "#descricao-img-" + index;
-				var tagDataImg = "#data-img-" + index;
-				var descricaoImg = data[index].explanation;
-				var videoPrinciapal = "#video-principal-"+index;
-				var videoLink = "#video-link-"+index;
-				var urlDayImgSemana = data[index].url;
-				descricaoImg = descricaoImg.slice(0, 150) + "...";
-				
-
-	  			if(urlDayImgSemana.includes("youtube")){
-					$(tagImg).css('display','none');
-
-					$(videoPrinciapal).css('display','');
-					$(videoLink).attr('src',data[index].url);
-
-					$(tagTituloImg).text(data[index].title);
-					$(tagDescricaoImg).text(descricaoImg);
-					$(tagDataImg).text(data[index].date);
-
-					console.log("preenchimento youtube");
-					console.log(videoPrinciapal);
-				}else{
-					$(tagImg).attr('src',data[index].url);
-	  				$(tagTituloImg).text(data[index].title);
-					$(tagDescricaoImg).text(descricaoImg);
-					$(tagDataImg).text(data[index].date);
-				}
-
-
-	  			
-
-	  		}
-	  	}
-	  })
-	  .catch(error => {
-	    // Trata erros de rede ou problemas na requisição
-	    console.error('There was a problem with the fetch operation:', error);
-	});
-
-	$('#link-post').click(
-		function(){
-		 navigator.clipboard.writeText("https://imagenday.vercel.app/")
-        .then(() => {
-            $('#link-post').text("Link Copiado!");
-            $('#link-post').css('background-color','#ef6383');// background-color: #ef3353
-        })
-        .catch(err => {
-            console.error('Falha ao copiar o texto: ', err);
-        });
 		}
-	);  
-})
+		
+		header .img-principal{
+			width: 50%;
+			height: 50%;
+		}
 
+		.inicio{
+			width: 100%;
+			height: auto;
+		}
 
-//<p class="descricao-pag" id="descricao-pag">"Todos os dias uma imagen do cosmo com uma breve descrição"</p>
+		.contanie-img{
+			grid-template-columns: 1fr;
+			grid-auto-flow: 1fr;
+			height: auto;
+			width: 100%;
+		}
+
+		.contanie-img .dados-img button{
+			margin-bottom: 10px;
+		}
+
+		.cards-img{
+			display: grid;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+			height: auto;
+		}
+
+		.cards-img .texto-titulo{
+			width: 100%;
+		}
+
+		.cards-blocos{
+			
+			height: auto;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+
+		.cards-blocos ul li{
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+			justify-content: center;
+			margin: 0px;			
+			margin-bottom: 10px;
+			position: relative;
+			left: -8%;
+		}	
+
+		.cards-blocos ul li img{
+			width: 95%;
+		}
+
+		.cards-blocos ul li .dados-img{
+			width: 100%;
+		}
+
+		footer i{
+			font: 24px;
+		}
+	
+		footer a, h3{
+			font-size: 12px;
+		}
+		
+	}
